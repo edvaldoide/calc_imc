abc47f4d37d73a0b66731afc572cc581b74f6076
Edvaldo Ide <edvaldoide@gmail.com>
2022-03-15 22:41:31 -0300

    Limpando o void main

--- calc_imc/lib/main.dart
+++ calc_imc/lib/main.dart
@@ -1,30 +1,4 @@
-import 'package:flutter/material.dart';
+void main () {
 
-void main() {
-  runApp(MaterialApp(
-    home: Home(),
-  ));
-}
 
-class Home extends StatefulWidget {
-  const Home({Key? key}) : super(key: key);
-
-  @override
-  State<Home> createState() => _HomeState();
-}
-
-class _HomeState extends State<Home> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Calculadora de IMC'),
-        centerTitle: true,
-        backgroundColor: Colors.green,
-        actions: [
-          IconButton (onPressed: () {}, icon: Icon(Icons.refresh))
-        ],
-      ),
-    );
-  }
 }
