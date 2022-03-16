d4070a4d6b298d9638f1d149318dde01a715cb7f
Edvaldo Ide <edvaldoide@gmail.com>
2022-03-15 21:38:14 -0300

    Adicionando Scaffold e Appbar

--- calc_imc/lib/main.dart
+++ calc_imc/lib/main.dart
@@ -1,5 +1,25 @@
-//Testando o VCS
+import 'package:flutter/material.dart';
 
-void main () {
+void main() {
+  runApp(MaterialApp(
+    home: Home(),
+  ));
+}
 
+class Home extends StatefulWidget {
+  const Home({Key? key}) : super(key: key);
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Calculadora de IMC'),
+      ),
+    );
+  }
 }
