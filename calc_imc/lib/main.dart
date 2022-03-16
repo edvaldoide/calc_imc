84e2898de3c9e7c1df9b0458f222787bdf4f21c1
Edvaldo Ide <edvaldoide@gmail.com>
2022-03-15 23:18:27 -0300

    Adicionando o TextField e efetuando Decorações

--- calc_imc/lib/main.dart
+++ calc_imc/lib/main.dart
@@ -21,8 +21,31 @@ class _HomeState extends State<Home> {
         title: Text('Calculadora de IMC'),
         centerTitle: true,
         backgroundColor: Colors.green,
-        actions: [
-          IconButton (onPressed: () {}, icon: Icon(Icons.refresh))
+        actions: [IconButton(onPressed: () {}, icon: Icon(Icons.refresh))],
+      ),
+      backgroundColor: Colors.white,
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          Icon(Icons.person_outline, size: 120.0, color: Colors.green),
+          TextField(
+            keyboardType: TextInputType.number,
+            decoration: InputDecoration(
+              labelText: 'Peso em (kg)',
+              labelStyle: TextStyle(color: Colors.green),
+            ),
+            textAlign: TextAlign.center,
+            style: TextStyle(color: Colors.green, fontSize: 25.0),
+          ),
+          TextField(
+            keyboardType: TextInputType.number,
+            decoration: InputDecoration(
+              labelText: 'Altura em (cm)',
+              labelStyle: TextStyle(color: Colors.green),
+            ),
+            textAlign: TextAlign.center,
+            style: TextStyle(color: Colors.green, fontSize: 25.0),
+          ),
         ],
       ),
     );
