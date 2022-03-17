256958bf246b9c443dba64fc252dee14717123f4
Edvaldo Ide <edvaldoide@gmail.com>
2022-03-16 23:49:08 -0300

    Adicionando o TextField e efetuando Decorações

--- calc_imc/lib/main.dart
+++ calc_imc/lib/main.dart
@@ -24,7 +24,30 @@ class _HomeState extends State<Home> {
         actions: [IconButton(onPressed: () {}, icon: Icon(Icons.refresh))],
       ),
       backgroundColor: Colors.white,
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          Icon(Icons.person_outline, size: 120.0, color: Colors.green),
+          TextField(
+            keyboardType: TextInputType.number,
+            decoration: InputDecoration(
+              labelText: 'Peso em (kg)',
+              labelStyle: TextStyle(color: Colors.green),
+            ),
+            textAlign: TextAlign.center,
+            style: TextStyle(color: Colors.green, fontSize: 25.0),
+          ),
+          TextField(
+            keyboardType: TextInputType.number,
+            decoration: InputDecoration(
+              labelText: 'Altura em (cm)',
+              labelStyle: TextStyle(color: Colors.green),
+            ),
+            textAlign: TextAlign.center,
+            style: TextStyle(color: Colors.green, fontSize: 25.0),
+          ),
+        ],
+      ),
     );
   }
 }
-
