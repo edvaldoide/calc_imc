7948aaa532ade99249da605c3cb4f2cb53be9d44
Edvaldo Ide <edvaldoide@gmail.com>
2022-03-16 23:11:40 -0300

    Apaguei o void main

--- calc_imc/lib/main.dart
+++ calc_imc/lib/main.dart
@@ -1,53 +1,5 @@
-import 'package:flutter/material.dart';
+void main () {
 
-void main() {
-  runApp(MaterialApp(
-    home: Home(),
-  ));
-}
 
-class Home extends StatefulWidget {
-  const Home({Key? key}) : super(key: key);
 
-  @override
-  State<Home> createState() => _HomeState();
-}
-
-class _HomeState extends State<Home> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Calculadora de IMC'),
-        centerTitle: true,
-        backgroundColor: Colors.green,
-        actions: [IconButton(onPressed: () {}, icon: Icon(Icons.refresh))],
-      ),
-      backgroundColor: Colors.white,
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        children: [
-          Icon(Icons.person_outline, size: 120.0, color: Colors.green),
-          TextField(
-            keyboardType: TextInputType.number,
-            decoration: InputDecoration(
-              labelText: 'Peso em (kg)',
-              labelStyle: TextStyle(color: Colors.green),
-            ),
-            textAlign: TextAlign.center,
-            style: TextStyle(color: Colors.green, fontSize: 25.0),
-          ),
-          TextField(
-            keyboardType: TextInputType.number,
-            decoration: InputDecoration(
-              labelText: 'Altura em (cm)',
-              labelStyle: TextStyle(color: Colors.green),
-            ),
-            textAlign: TextAlign.center,
-            style: TextStyle(color: Colors.green, fontSize: 25.0),
-          ),
-        ],
-      ),
-    );
-  }
 }
