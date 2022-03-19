aedca0b74b4b41887f9ddf907b858ac9b6b30e83
Edvaldo Ide <edvaldoide@gmail.com>
2022-03-18 22:07:58 -0300

    Correção da tabela de IMC

--- calc_imc/lib/main.dart
+++ calc_imc/lib/main.dart
@@ -30,15 +30,15 @@ class _HomeState extends State<Home> {
       double weight = double.parse(weightController.text);
       double height = double.parse(heightController.text) / 100;
       double imc = weight / (height * height);
-      if (imc < 18.6) {
+      if (imc < 18.5) {
         _infotext = "Abaixo do Peso (${imc.toStringAsPrecision(3)})";
-      } else if (imc >= 18.6 && imc < 24.9) {
+      } else if (imc >= 18.5 && imc < 25) {
         _infotext = "Peso Ideal (${imc.toStringAsPrecision(3)})";
-      } else if (imc >= 24.9 && imc < 29.9) {
+      } else if (imc >= 25 && imc < 30) {
         _infotext = "Levemente acima do Peso (${imc.toStringAsPrecision(3)})";
-      } else if (imc >= 29.9 && imc < 34.9) {
+      } else if (imc >= 30 && imc < 35) {
         _infotext = "Obesidade Grau I (${imc.toStringAsPrecision(3)})";
-      } else if (imc >= 34.9 && imc < 39.9) {
+      } else if (imc >= 35 && imc < 40) {
         _infotext = "Obesidade Grau II (${imc.toStringAsPrecision(3)})";
       } else if (imc >= 40) {
         _infotext = "Obesidade Grau III (${imc.toStringAsPrecision(3)})";
